b1ab27c7d678013f14d0dcf4ddb42d6dc2e2f7c1
ouripoupko <ouri.poupko@gmail.com>
2024-03-31 00:17:27 +0300

    login page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', children: [
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    { path: 'login', component: LoginComponent },
+    { path: 'main', component: MainComponent }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
