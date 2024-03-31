21ddc7ee40e3582b79af3e259239091a4ef9919a
ouripoupko <ouri.poupko@gmail.com>
2024-03-31 11:31:51 +0300

    login page design almost complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { MainComponent } from './main/main.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AgentService } from '../agent.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  key: string = '';
+  step: number = 1;
+  selectedServer: string = '';
+  registerChecked: boolean = false;
+
+  constructor(
+    private router: Router,
+    private agentService: AgentService,
+  ) { }
+
+  onFileSelected(event: any) {
+    if(event.target.files.length > 0)
+    {
+      var reader = new FileReader();
+      reader.onload = () => {
+        var text = reader.result as string;
+        var json = JSON.parse(text);
+        this.key = json['public'];
+        this.step = 2;
+      };
+      reader.readAsText(event.target.files[0]);
+    }
+  }
+
+  onGenerate(event: any) {
+    var pad = new Uint8Array(32);
+    window.crypto.getRandomValues(pad);
+    let publicKey = Array.from(pad, value => value.toString(16).padStart(2, '0')).join('');
+    const pair = {'public': publicKey, 'private': 'This is not a real key pair. just a mockup.'};
+    const blob = new Blob([JSON.stringify(pair, null, 2)], { type: "application/json",});
+    var url = window.URL.createObjectURL(blob);
+    var anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = "my_key.json";
+    anchor.click();
+    window.URL.revokeObjectURL(url);
+    this.key = publicKey;
+    this.step = 2;
+  }
+
+  location(full: boolean) {
+    return full ? location.origin : location.hostname + (location.port ? ' (' + location.port + ')' : '');
+  }
+
+  onServerSelected(event: any) {
+    this.step = 3;
+  }
+
+  navigate(server: string) {
+    this.router.navigate(['main'], { queryParams: { server: server, agent: this.key}});
+  }
 
 }
