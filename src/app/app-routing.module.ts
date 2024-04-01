fa27508e9d83d3cf7b2ff973ab8aaf2e04da2799
ouripoupko <ouri.poupko@gmail.com>
2024-04-01 19:47:45 +0300

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
+import { ScanComponent } from './scan/scan.component';
+import { CommunitiesComponent } from './communities/communities.component';
+import { CommunityComponent } from './community/community.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', children: [
     { path: '', pathMatch: 'full', redirectTo: 'login' },
     { path: 'login', component: LoginComponent },
-    { path: 'main', component: MainComponent }
+    { path: 'main', component: MainComponent },
+    { path: 'scan', component: ScanComponent},
+    { path: 'communities', component: CommunitiesComponent},
+    { path: 'community', component: CommunityComponent},
+    { path: 'profile', component: ProfileComponent}
   ]}
 ];
 
